2d3a66fd63801c8f2a67559bdb779d26643aa37f
mousheraa gomaa <mousheraa.gomaa@gmail.com>
2025-12-07 12:12:36 +0200

    edit ui

---
+++ assets/js/header.js
@@ -0,0 +1,6 @@
+  if($('.menu-trigger').length){
+    $(".menu-trigger").on('click', function() { 
+      $(this).toggleClass('active');
+      $('.header-area .nav').slideToggle(200);
+    });
+  }
